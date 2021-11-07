e919d2307588270a2701db2cdc575c67497803ff
Karolina <k.karo.ty@gmail.com>
2021-11-07 15:38:47 +0100

    Initial project structure

---
+++ src/main/java/simulation/World.java
@@ -0,0 +1,22 @@
+package simulation;
+
+import java.util.Random;
+
+public class World {
+
+	private static final Random random = new Random();
+	private static final Animal animal = new Animal(new Vector2D(random.nextInt(100),
+			random.nextInt(100)));
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+		run();
+		System.out.println("Stop");
+	}
+
+	private static void run () {
+		for (int i = 0; i < 10; i++) {
+			animal.move(MapDirection.values()[random.nextInt(MapDirection.values().length)]);
+		}
+	}
+}
